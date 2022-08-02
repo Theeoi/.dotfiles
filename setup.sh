e09f29d4ae5782ff192f61b4c666c8f26731d890
Theodor Blom <me@theodorblom.com>
2022-08-02 18:16:51 +0200

    Moved gitconfig symlink

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 # Create hard links
 ln -s ~/.dotfiles/home/bashrc ~/.bashrc
 ln -s ~/.dotfiles/home/vimrc ~/.vimrc
-ln -s ~/.dotfiles/home/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/home/gitconfig ~/.config/git/config
 ln -s ~/.dotfiles/home/config/nvim/lua/user/init.lua ~/.config/nvim/lua/user/init.lua
